545c149c5b9b77e47f4e8c869ffdedb6127c8128
Piotr Ludynia <pietrekpl18@gmail.com>
2023-06-15 11:01:07 +0200

    initial commit

---
+++ src/main/java/toDoAppRecruitmentTask/toDoAppRecruitmentTask/ToDoAppRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package toDoAppRecruitmentTask.toDoAppRecruitmentTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppRecruitmentTaskApplication.class, args);
+	}
+
+}
